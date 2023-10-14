721d85ad41ec14dd227549e985db50d92a6a3c77
Harsh Manvar <harsh-manvar@users.noreply.huggingface.co>
2023-10-14 19:49:22 +0000

    Update model.py

--- model.py
+++ model.py
@@ -9,14 +9,14 @@ from transformers import TextIteratorStreamer, AutoTokenizer
 logging.set_verbosity_info()
 logger = logging.get_logger("transformers")
 
-config = {'max_new_tokens': 256, 'repetition_penalty': 1.1, 
-          'temperature': 0.1, 'stream': True}
-model_id = 'TheBloke/Llama-2-7B-Chat-GGML'
+config = {"max_new_tokens": 256, "repetition_penalty": 1.1, 
+          "temperature": 0.1, "stream": True}
+model_id = "TheBloke/Llama-2-7B-Chat-GGML"
 device = "cpu"
 
 
-model = AutoModelForCausalLM.from_pretrained(model_id, model_type="llama", lib='avx2', hf=True)
-tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-2-7b-chat-hf')
+model = AutoModelForCausalLM.from_pretrained(model_id, model_type="llama", lib="avx2", hf=True)
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
 
 def get_prompt(message: str, chat_history: list[tuple[str, str]],
                system_prompt: str) -> str:
@@ -75,5 +75,6 @@ def run(message: str,
 
     outputs = []
     for text in streamer:
+        logger.info("outputs", outputs)
         outputs.append(text)
-        yield ''.join(outputs)
+        yield "".join(outputs)
